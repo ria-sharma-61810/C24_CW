d107fd4a244cfadf850111d35dbf39f4e3866b18
ria-sharma-61810 <91355915+ria-sharma-61810@users.noreply.github.com>
2021-12-23 11:38:06 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+var balls = [];
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+  cannon = new Cannon(180, 110, 130, 100, angle);
+
+  
+  
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    cannonBall = new CannonBall(cannon.x, cannon.y);
+    cannonBall.shoot();
+    
+    balls.push(cannonBall)
+    console.log(balls)
+  }
+}
+
+// function keyReleased(){
+//   if(keyCode === DOWN_ARROW){
+//     //ball[balls.length-1].shoot()
+//   }
+// }
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+
+  push();
+  fill("brown");
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+  cannon.display();
+  for(var i = 0;i<balls.length;i++){
+    showBalls(balls[i],i);
+  }
+  
+  
+}
+
+function showBalls(balls,i){
+  if(balls){
+    balls.display()
+  }
+}
+
+
